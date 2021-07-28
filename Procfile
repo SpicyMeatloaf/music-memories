afde3087ec685d55b8a7180f9fbe4dfe91ac98a3
Niladri <niladri.sinha89@gmail.com>
2021-07-27 21:30:06 -0400

    prepping for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn music-memories.wsgi
--- music_memories/settings.py
+++ music_memories/settings.py
@@ -128,3 +128,7 @@ LOGOUT_REDIRECT_URL = '/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import django_heroku
+django_heroku.settings(locals())
